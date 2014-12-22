03e6d04cc74eff52dc2452d27941c594b7a016ae
Marc Scholten <marc@pedigital.de>
2014-12-22 13:55:17 +0100

    Cleanup of test suite

--- spec/host_manager_spec.rb
+++ spec/host_manager_spec.rb
@@ -1,12 +1,6 @@
 require 'host_manager.rb'
 require 'tempfile'
 
-RSpec.configure do |config|
-  config.expect_with :rspec do |c|
-    c.syntax = :should
-  end
-end
-
 def fixture(name)
   File.join(File.dirname(__FILE__), 'fixtures', name.to_s)
 end
@@ -14,12 +8,12 @@ end
 describe HostManager, "#patch" do
   it "returns nil if no changes" do
     host_manager = HostManager.new(fixture(:example_hosts), [], 'tests', '127.0.0.1')
-    host_manager.patch.should eq(nil)
+    expect(host_manager.patch).to eq(nil)
   end
 
   it "migrates existing hosts to own section" do
     host_manager = HostManager.new(fixture(:example_hosts), %w(app.example.lo www.example.lo example.lo), 'tests', '127.0.0.1')
-    host_manager.patch.to_s.should eq(
+    expect(host_manager.patch.to_s).to eq(
       "127.0.0.1 localhost\n" +
       "\n" +
       "# -----BEGIN SECTION maphosts - tests-----\n" +
@@ -31,27 +25,12 @@ describe HostManager, "#patch" do
   end
 
   it "updates hosts pointing to the wrong ip" do
-    hosts = Tempfile.new('hosts')
-
-    begin
-      hosts << (
-        "127.0.0.1 localhost\n" +
-        "# -----BEGIN SECTION maphosts - tests-----\n" +
-        "127.0.0.1 www.example.lo\n" +
-        "# -----END SECTION maphosts - tests-----\n"
-      )
-      hosts.rewind
-
-      host_manager = HostManager.new(hosts.path, %w(www.example.lo), 'tests', '127.0.0.2')
-      host_manager.patch.to_s.should eq(
-        "127.0.0.1 localhost\n" +
-        "# -----BEGIN SECTION maphosts - tests-----\n" +
-        "127.0.0.2 www.example.lo\n" + 
-        "# -----END SECTION maphosts - tests-----"
-      )
-    ensure
-      hosts.close
-      hosts.unlink
-    end
+    host_manager = HostManager.new(fixture(:hosts_wrong_ip), %w(www.example.lo), 'tests', '127.0.0.2')
+    expect(host_manager.patch.to_s).to eq(
+      "127.0.0.1 localhost\n" +
+      "# -----BEGIN SECTION maphosts - tests-----\n" +
+      "127.0.0.2 www.example.lo\n" + 
+      "# -----END SECTION maphosts - tests-----"
+    )
   end
 end
